467ce3f7431552623ef207691749ef0e70a38a9d
Prince Sehrawat <princesehrawat089@gmail.com>
2025-03-20 10:45:52 +0530

    save

--- src/test/exe1/Lab1.java
+++ src/test/exe1/Lab1.java
@@ -2,6 +2,6 @@ package test.exe1;
 
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello,AB World!");
     }
 }
